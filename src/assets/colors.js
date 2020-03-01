01a34fd0eef0e74718c65d193238dfc1d5b14bec
Hygor <hygor@x-apps.com.br>
2020-03-01 16:08:12 -0300

    feat(app): adding assets

---
+++ src/assets/colors.js
@@ -0,0 +1,14 @@
+export default {
+  primary: '#EE7600',
+  secondary: '#F0445A',
+  textPrimary: '#FFFFFF',
+  textSecondary: '#BCBCBC',
+  textLight: '#A09F9D',
+  inactive: '#8C8D91',
+  // background: '#141418',
+  background: '#141418',
+  paper: '#212127',
+  error: '#EE002B',
+  errorOverlay: 'rgba(238,0,43,0.2)',
+  separator: '#34343E',
+};
