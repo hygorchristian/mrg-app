3c79daaad80b51eea6570521780723e856f3a1e6
Hygor <hygor@x-apps.com.br>
2020-03-02 20:48:11 -0300

    feat(search): create search screen

--- src/screens/Search/Search.js
+++ src/screens/Search/Search.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  Form,
+  CloseButton,
+  CloseIcon,
+  Header,
+  ResultList,
+  ResultSeparator,
+  SearchIcon,
+  SearchInput,
+} from './styles';
+import { useNavigation } from 'react-navigation-hooks';
+import ResultItem from '~/components/ResultItem';
+import { useSelector } from 'react-redux';
 
 function Search() {
-  return <Container />;
+  const [items, setItems] = useState([]);
+  const [query, setQuery] = useState('');
+  const podcasts = useSelector(state => state.podcasts.data);
+  const navigation = useNavigation();
+
+  const fechar = () => {
+    navigation.pop();
+  };
+
+  useEffect(() => {
+    if (query.length === 0) {
+      setItems([]);
+      return;
+    }
+
+    const filtered = podcasts.filter(item => {
+      if (
+        item.title.toLowerCase().includes(query.toLowerCase()) ||
+        item.description.toLowerCase().includes(query.toLowerCase()) ||
+        item.number.toLowerCase().includes(query.toLowerCase())
+      ) {
+        return item;
+      }
+    });
+
+    setItems(filtered);
+  }, [query]);
+
+  return (
+    <Container>
+      <Header>
+        <CloseButton onPress={fechar}>
+          <CloseIcon />
+        </CloseButton>
+      </Header>
+      <Form>
+        <SearchIcon />
+        <SearchInput
+          onChangeText={val => setQuery(val)}
+          value={query}
+          placeholder="Digite sua busca ..."
+          autoFocus
+        />
+      </Form>
+      <ResultList
+        data={items}
+        keyExtractor={item => item.uid}
+        renderItem={({ item }) => <ResultItem item={item} />}
+        ItemSeparatorComponent={() => <ResultSeparator />}
+      />
+    </Container>
+  );
 }
 
 export default Search;
--- src/screens/Search/styles.js
+++ src/screens/Search/styles.js
@@ -1,5 +1,116 @@
 import styled from 'styled-components/native';
+import { Platform, StyleSheet } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import LinearGradient from 'react-native-linear-gradient';
+import MDIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export const Container = styled.View`
-  
+import Text from '~/components/Montserrat';
+import { SEMIBOLD } from '~/components/Montserrat/fonts';
+import colors from '~/assets/colors';
+
+const PADDING_TOP = Platform.select({
+  ios: getStatusBarHeight() + 12,
+  android: 12,
+});
+
+const MARGIN_TOP = Platform.select({
+  ios: -2,
+  android: -5,
+});
+
+export const Container = styled(LinearGradient).attrs({
+  colors: [colors.background, colors.background],
+})`
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  padding-top: ${PADDING_TOP};
+`;
+
+export const Header = styled.View`
+  height: 55px;
+  align-items: center;
+  padding-horizontal: 20px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const CloseButton = styled.TouchableOpacity`
+  height: 55px;
+  width: 55px;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const CloseIcon = styled(MDIcon).attrs({
+  size: 24,
+  name: 'close',
+})`
+  color: white;
+`;
+
+export const StoreInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const StoreLabel = styled(Text).attrs({
+  weight: 'medium',
+})`
+  font-size: 14px;
+  font-weight: 600;
+  color: white;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  margin-top: 65px;
+  padding-horizontal: 35px;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const SearchIcon = styled(MDIcon).attrs({
+  size: 28,
+  name: 'magnify',
+})`
+  color: white;
+  margin-right: 14px;
+  margin-top: ${MARGIN_TOP};
+`;
+
+export const SearchInput = styled.TextInput.attrs({
+  placeholderTextColor: 'rgba(255,255,255,0.5)',
+  selectionColor: 'white',
+})`
+  font-size: 21px;
+  font-weight: 600;
+  color: white;
+  height: 44px;
+  flex: 1;
+  font-family: ${SEMIBOLD};
+`;
+
+export const ResultList = styled.FlatList`
+  margin-top: 40px;
+  padding-horizontal: 20px;
+  flex: 1;
+`;
+
+export const ResultSeparator = styled.View`
+  width: 100%;
+  height: ${StyleSheet.hairlineWidth};
+  background-color: rgba(255, 255, 255, 0.2);
+`;
+
+export const ResultButton = styled.TouchableOpacity`
+  width: 100%;
+  height: 48px;
+  padding-horizontal: 15px;
+  justify-content: center;
+`;
+
+export const ResultItem = styled(Text)`
+  color: white;
+  font-size: 14px;
 `;
